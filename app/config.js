4ca8377bb1e7946494ab26f890bfc666a53c1791
vickytnz <vicky.teinaki@gmail.com>
2024-10-20 20:03:46 +0100

    add existing page changes

--- app/config.js
+++ app/config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit documentation',
-
+//  serviceName: 'NHS.UK prototype kit documentation',
+serviceName: 'Get a test to check if you have magical powers',
   // Port to run nodemon on locally
   port: 2000,
 

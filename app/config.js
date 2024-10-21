8f6a091832ef3f7318c4377d614ea8f82ae11445
Vicky Teinaki <vicky.teinaki@gmail.com>
2024-10-21 16:44:22 +0100

    Update app/config.js
    Co-authored-by: Frankie Roberto <frankie@frankieroberto.com>

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
- serviceName: 'NHS.UK prototype kit documentation',
+  serviceName: 'NHS.UK prototype kit documentation',
   // Port to run nodemon on locally
   port: 2000,
 

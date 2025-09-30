64fd96c85c11b1e6fd0997cf08b04d8ccd493adc
Frankie Roberto <frankie.roberto1@nhs.net>
2025-09-30 14:38:52 +0100

    Update Get started page and initial flow (#353)
    This changes the Get started page to now use 4 cards for the 4 different
    install guides:
    
    * Mac
    * Windows
    * Codespaces
    * Advanced
    
    This hopefully makes the Codespaces guide more discoverable, now that
    we’re more confidence that it’s a viable option for users.
    
    The updated page also removes the radio branching question to get to the
    Mac on Windows guides, as these can be directly linked to now instead.
    
    Thanks to @paulrobertlloyd for suggesting this in #349
    
    ---------
    
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>

--- app/routes.js
+++ app/routes.js
@@ -9,17 +9,6 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-// Install - Mac or Windows branching
-router.post('/install/mac', (req, res) => {
-  const { machine } = req.session.data;
-
-  if (machine === 'Mac') {
-    res.redirect('/install/mac/terminal');
-  } else if (machine === 'Windows') {
-    res.redirect('/install/windows/terminal');
-  } else res.redirect('/install/mac-or-windows');
-});
-
 router.get('/download', (req, res) => {
   const { prototypeKitVersion } = packageJson;
   res.redirect(

45de40113554738cb21575e57103a9e64bec70ee
Frankie Roberto <frankie@frankieroberto.com>
2025-09-30 20:31:00 +0100

    Remove previous dynamic site code

--- app/config.js
+++
@@ -1,9 +0,0 @@
-// Use this file to change prototype configuration.
-
-module.exports = {
-  // Service name
-  serviceName: 'Prototype kit',
-  // Port to run nodemon on locally
-  port: 2000,
-
-};
